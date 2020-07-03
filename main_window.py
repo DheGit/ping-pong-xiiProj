b46c349d7dca6d677b042da186fda09da1fb67ab
DheGit <kevindheer@gmail.com>
2020-07-03 12:52:23 +0530

    Added the basic skeleton for the main window, will build on that

--- main_window.py
+++ main_window.py
@@ -1,1 +1,21 @@
 """This file encloses the code for the main window which is displayed when the program starts, which includes the options for starting game, settings, and more"""
+#TODO: Add conditionals to the imports to enable cross-platform compatibility
+import tkinter as TK
+
+#Components are stacked together, buttons in a group, labels in another, etc. Naming convention for component Objects: camelCaseNaming
+
+#check StringVar(), it's causing errors
+mainContainer=TK.Tk()
+
+testLabel_text=TK.StringVar()
+
+testLabel=TK.Label(mainContainer, textvariable=testLabel_text, relief=TK.RAISED)
+
+
+def start_intro_window():
+	testLabel_text.set("Everything looks good for now")
+	testLabel.pack()
+
+	mainContainer.mainloop()
+
+start_intro_window()
