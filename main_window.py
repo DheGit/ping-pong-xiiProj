1e898175b7491588c64ff17b100b2eb4ffaccaa3
DheGit <kevindheer@gmail.com>
2020-07-06 08:32:43 +0530

    Added a quit button, added it's text in resources.py, changed label formatting

--- main_window.py
+++ main_window.py
@@ -8,15 +8,24 @@ from resources import *
 mainContainer=TK.Tk()
 
 testLabel_text=TK.StringVar()
-testLabel=TK.Label(mainContainer, textvariable=testLabel_text, relief=TK.RAISED)
+testLabel=TK.Label(mainContainer, textvariable=testLabel_text, relief=TK.SUNKEN)
 
-mainContainer.minsize(window_height, window_width)
+#The callback method for buttons must come before the definition of the buttons as it uses the callback method name
+def onClick_quitButton():
+	quit()
+quitButton=TK.Button(mainContainer, text=r_quit_button_txt, command=onClick_quitButton)
 
 """init_layout(): Contains the initialisation code for the layout, including the styling"""
 def init_layout():
-	global testLabel_text, testLabel
+	global testLabel_text, testLabel, mainContainer
+	mainContainer.minsize(window_height, window_width)
+	
 	testLabel_text.set(r_test_label_txt)
 	testLabel.pack(side=TK.TOP, padx=10, pady=10)
+
+	global quitButton
+	quitButton.pack(side=TK.TOP)
+
 
 def start_intro_window():
 	init_layout()
--- resources.py
+++ resources.py
@@ -3,3 +3,5 @@ window_height=150
 
 r_test_label_txt='''Everything looks good for now
 		Yup'''
+
+r_quit_button_txt="Quit"
