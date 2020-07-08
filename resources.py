3467afbd014869c2e5ed6b533b7b6310794137b5
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-07-08 22:11:10 +0530

    Update resources.py

--- resources.py
+++ resources.py
@@ -1,12 +1,11 @@
 window_width=150
 window_height=150
 
-r_test_label_txt='''Everything looks good for now
-		Yup'''
+r_test_label_txt='''Everything looks good for now'''
 
 r_title_label_txt="Ping-Pong"
-r_p1_name_label_txt="Enter the name of Player1"
-r_p2_name_label_txt="Enter the name of Player2"
+r_p1_name_label_txt="Enter the name of Player 1"
+r_p2_name_label_txt="Enter the name of Player 2"
 
 r_quit_button_txt="Quit"
 r_start_button_txt="Start"
