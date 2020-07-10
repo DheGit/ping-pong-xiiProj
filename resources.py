4d13c1706313a93d9091fb1d8a1c861894949494
DheGit <kevindheer@gmail.com>
2020-07-10 22:06:12 +0530

    Renamed the second menu

--- resources.py
+++ resources.py
@@ -2,8 +2,8 @@ window_width=150
 window_height=150
 
 r_test_label_txt='''Everything looks good for now'''
-
 r_title_label_txt="Ping-Pong"
+
 r_p1_name_label_txt="Enter the name of Player 1"
 r_p2_name_label_txt="Enter the name of Player 2"
 
