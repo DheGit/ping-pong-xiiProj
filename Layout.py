0ed7abbe48d4863d139832d12f7683305e512288
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-07-13 15:51:57 +0530

    Create Layout.py
    Has some error with the paddles but I am not getting it
    Please see to it

---
+++ Layout.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+black = (0,0,0)
+white = (255,255,255)
+
+size = (900,600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Ping Pong")
+
+screen.fill(black)
+pygame.draw.line(screen,white,[449,0],[449,600],8)
+pygame.display.flip()
+
+class Paddle(pygame.sprite.Sprite):
+    
+    def __init__(self,color,width,height):
+        
+        super().__init__()
+        
+        self.image = pygame.Surface([width,height])
+        self.image.fill(white)
+        
+        pygame.draw.rect(self.image,color,[0,0,width,height])
+        
+        self.rect = self.image.get_rect()
+
+paddle1 = Paddle(white,10,100)
+paddle1.rect.x = 20
+paddle1.rect.y = 200
+
+paddle2 = Paddle(white,10,100)
+paddle2.rect.x = 670
+paddle2.rect.y = 200
