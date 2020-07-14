ab76a8b0f0aefda65e781db70aefba73d3874f3e
DheGit <kevindheer@gmail.com>
2020-07-14 17:17:05 +0530

    Changed hardcoded values to variables to make code consistent, added a main game loop

--- Layout.py
+++ Layout.py
@@ -2,16 +2,22 @@ import pygame
 
 pygame.init()
 
-black = (0,0,0)
-white = (255,255,255)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
 
-size = (900,600)
+SCREEN_WIDTH=900
+SCREEN_HEIGHT=600
+PADDLE_WIDTH=12
+PADDLE_HEIGHT=120
+PADDLE_MARGIN=20
+
+size = (SCREEN_WIDTH,SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Ping Pong")
 pygame.mouse.set_visible(0)
 
-screen.fill(black)
-pygame.draw.line(screen,white,[449,0],[449,600],8)
+screen.fill(BLACK)
+pygame.draw.line(screen,WHITE,[SCREEN_WIDTH//2,0],[SCREEN_WIDTH//2,SCREEN_HEIGHT],4)
 
 class Paddle(pygame.sprite.Sprite):
     
@@ -19,18 +25,18 @@ class Paddle(pygame.sprite.Sprite):
         
         super().__init__()
         
-        self.image = pygame.Surface([12,100])
-        self.image.fill(white)
+        self.image = pygame.Surface([PADDLE_WIDTH,PADDLE_HEIGHT])
+        self.image.fill(WHITE)
         
         self.rect = self.image.get_rect()
 
 paddle1 = Paddle()
-paddle1.rect.x = 20
-paddle1.rect.y = 250
+paddle1.rect.x = PADDLE_MARGIN
+paddle1.rect.y = SCREEN_HEIGHT//2 - PADDLE_HEIGHT//2
 
 paddle2 = Paddle()
-paddle2.rect.x = 870
-paddle2.rect.y = 250
+paddle2.rect.x = SCREEN_WIDTH - PADDLE_WIDTH - PADDLE_MARGIN
+paddle2.rect.y = SCREEN_HEIGHT//2 - PADDLE_HEIGHT//2
 
 movingsprites = pygame.sprite.Group()
 movingsprites.add(paddle1)
@@ -39,3 +45,13 @@ movingsprites.add(paddle2)
 movingsprites.draw(screen)
 
 pygame.display.flip()
+
+exit_window=False
+
+while not exit_window:
+
+	for event in pygame.event.get():
+		if event.type==pygame.QUIT:
+			exit_window=True
+
+pygame.quit()
