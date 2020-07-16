302e6b3c9d011dc87cf85986f07ba87e83e8ee9d
DheGit <kevindheer@gmail.com>
2020-07-16 21:50:42 +0530

    The ball now bounces

--- Layout.py
+++ Layout.py
@@ -9,9 +9,12 @@ WHITE = (255,255,255)
 
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
+
 PADDLE_WIDTH = 12
 PADDLE_HEIGHT = 120
 PADDLE_MARGIN = 20
+PADDLE_BOUNCE_BIAS=80
+
 BALL_WIDTH = 17
 BALL_HEIGHT = 17
 BALL_RESET_Y_MARGIN=50
@@ -53,6 +56,10 @@ class Ball(pygame.sprite.Sprite):
 
         self.reset()
 
+    def bounce(self, b_param):
+    	self.direction = (180-self.direction)%360
+    	self.direction += (b_param/PADDLE_HEIGHT)*PADDLE_BOUNCE_BIAS
+
     def reset(self):
     	self.speed=8.0
     	self.y=random.randrange(BALL_RESET_Y_MARGIN, SCREEN_HEIGHT - BALL_RESET_Y_MARGIN)
@@ -70,11 +77,14 @@ class Ball(pygame.sprite.Sprite):
     	self.x += math.cos(rads) * self.speed
     	self.y -= math.sin(rads) * self.speed
 
-    	if self.x < 0 or self.x > SCREEN_WIDTH:
+    	if self.x < -BALL_WIDTH*20 or self.x > SCREEN_WIDTH + BALL_WIDTH*20:
     		self.reset()
 
     	self.rect.x=int(self.x)
     	self.rect.y=int(self.y)
+
+    	if self.y<=0 or self.y>=SCREEN_HEIGHT - BALL_HEIGHT:
+    		self.direction = (360-self.direction)%360
 
 clock=pygame.time.Clock()
 
@@ -90,6 +100,16 @@ ball = Ball()
 ball.reset()
 ball.update()
 
+ball_group = pygame.sprite.Group()
+ball_group.add(ball)
+
+def collides():
+	if (ball.x <= PADDLE_MARGIN + PADDLE_WIDTH and ball.x >= PADDLE_MARGIN + PADDLE_WIDTH - ball.speed*2) and (ball.y >= paddle1.rect.y and ball.y <=paddle1.rect.y + PADDLE_HEIGHT):
+		return 1
+	if (ball.x >= SCREEN_WIDTH - (PADDLE_MARGIN + BALL_WIDTH) and ball.x <= SCREEN_WIDTH - (PADDLE_MARGIN + BALL_WIDTH) + ball.speed*2)and (ball.y >= paddle2.rect.y and ball.y <=paddle2.rect.y + PADDLE_HEIGHT):
+		return 2
+	return 0
+
 movingsprites = pygame.sprite.Group()
 movingsprites.add(paddle1)
 movingsprites.add(paddle2)
@@ -98,18 +118,27 @@ movingsprites.add(ball)
 exit_window=False
 
 while not exit_window:
-
+	screen.fill(BLACK)
+	
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
 			exit_window=True
 
 	ball.update()
-	print("Ball Speed: ", ball.speed)
 	
-	screen.fill(BLACK)
+	if collides() == 1:
+		diff = (paddle1.rect.y + PADDLE_HEIGHT/2) - (ball.rect.y+BALL_HEIGHT/2)
+		ball.x = PADDLE_MARGIN+PADDLE_WIDTH +1
+		ball.bounce(diff)
+
+	if collides() == 2:
+		diff = (paddle2.rect.y + PADDLE_HEIGHT/2) - (ball.rect.y+BALL_HEIGHT/2) 
+		ball.x = SCREEN_WIDTH - (PADDLE_MARGIN+BALL_WIDTH+1)
+		ball.bounce(-diff)
+
 	movingsprites.draw(screen)
 	pygame.display.flip()
 
-	clock.tick(30)
+	clock.tick(100) #Kept it that high only for testing purposes, change it to a comfortable 30-40 later.
 
 pygame.quit()
