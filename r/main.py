df050f2bee7e7af62957f7598891f7c3a292ec87
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-07-28 12:58:20 +0530

    Update main.py

--- r/main.py
+++ r/main.py
@@ -1,13 +1,15 @@
 window_width=150
 window_height=150
 
-r_test_label_txt='''Everything looks good for now'''
-r_title_label_txt="Ping-Pong"
+r_test_label_txt = '''Everything looks good for now'''
+r_title_label_txt = "Ping-Pong"
 
-r_p1_name_label_txt="Enter the name of Player 1"
-r_p2_name_label_txt="Enter the name of Player 2"
+r_p1_name_label_txt = "Enter the name of Player 1 :"
+r_p2_name_label_txt = "Enter the name of Player 2 :"
+r_playernames_label_txt = "Player Names"
 
-r_quit_button_txt="Quit"
-r_start_button_txt="Start"
+r_quit_button_txt = "Quit"
+r_start_button_txt = "Start"
+r_enter_button_txt = "Enter"
 
-r_coming_up_title="Under Development"
+r_coming_up_title = "Under Development"
