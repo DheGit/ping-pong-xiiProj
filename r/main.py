c73b68cf73927e98a1e925633d9229edf963019a
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-07-30 13:07:09 +0530

    Update main.py

--- r/main.py
+++ r/main.py
@@ -1,7 +1,3 @@
-window_width=150
-window_height=150
-
-r_test_label_txt = '''Everything looks good for now'''
 r_title_label_txt = "Ping-Pong"
 
 r_p1_name_label_txt = "Enter the name of Player 1 :"
@@ -13,5 +9,3 @@ r_start_button_txt = "Start"
 r_enter_button_txt = "Enter"
 
 r_playagain_button_txt = "Play Again"
-
-r_coming_up_title = "Under Development"
