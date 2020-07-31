657cfd55bce38d0b54d63a0fc2c198cc880dc9a3
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-07-31 19:42:20 +0530

    Update __init__.py

--- screens/__init__.py
+++ screens/__init__.py
@@ -2,6 +2,7 @@ from . import game
 from . import pause
 from . import endgame
 from . import main_menu
+from . import playernames
 #TODO Consider adding even the menu screen to a class so that the main code 'The Game.py' is even less concerned about the details of a screen
 
-__all__ = ['game', 'pause', 'endgame', 'main_menu']
+__all__ = ['game', 'pause', 'endgame', 'main_menu', 'playernames']
