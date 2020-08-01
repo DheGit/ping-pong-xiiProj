461470041c07a7fc637dac9c769e15ac0d0217b7
DheGit <kevindheer@gmail.com>
2020-08-01 11:39:47 +0530

    made the basic screen

--- endgame.py
+++ endgame.py
@@ -7,6 +7,11 @@ import pygame
 from sprites.UIElement import *
 import r
 
+CB_RETURN = 0
+CB_PAUSE = 1
+CB_ENDGAME = 2
+CB_QUIT = -1
+
 class EndgameScreen():
 
 	def __init__(self, screen):
@@ -18,9 +23,9 @@ class EndgameScreen():
 
 	def showEndScreen(self):
 		winner_label = UIElement(
-	        center_position=(r.game.SCREEN_WIDTH/2, 450), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
+	        center_position=(r.game.SCREEN_WIDTH/2, 200), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
 	        font_size=30,
-	        bg_rgb=r.colors.BLACK,
+	        bg_rgb=r.colors.BLUE,
 	        text_rgb=r.colors.WHITE,
 	        text=self.winnerName + r.endgame.win_statement,
 		)
@@ -29,18 +34,39 @@ class EndgameScreen():
 
 		ui_els = [winner_label]
 
+
 		while True:
+			mouse_up = False
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					return CB_QUIT
+				if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+					mouse_up = True
+
+			self.screen.fill(r.colors.BLUE)
 			for ui_el in ui_els:
 				ui_action=ui_el.update(pygame.mouse.get_pos(), False) #Change that appropriately
 				if ui_action is not None:
 					return ui_action
 				ui_el.draw(self.screen)
 
+			keys=pygame.key.get_pressed()
+			if keys[pygame.K_ESCAPE]:
+				return CB_QUIT
+
+			pygame.display.flip()
+
+
 if __name__=="__main__":
+	#Testing
 	pygame.init()
 	screen=pygame.display.set_mode((r.game.SCREEN_WIDTH, r.game.SCREEN_HEIGHT))
 
 	egScreen=EndgameScreen(screen)
-	egScreen.setWinnerName("Dheer The Banker")
+	egScreen.setWinnerName("Player1")
 
-	egScreen.showEndScreen()
+	new_status = egScreen.showEndScreen()
+
+	if new_status == CB_QUIT:
+		pygame.quit()
+	pygame.quit()
