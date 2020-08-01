6d0637458c14cfe7916187ffc2fbcbe898628ea1
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-01 15:51:13 +0530

    Update pause.py

--- screens/pause.py
+++ screens/pause.py
@@ -1,3 +1,50 @@
-#A dummy file to represent how the screens are going to be organised
+import pygame
 
-#TODO: Add code for pause here
+import r
+
+from sprites.UIElement import *
+
+CB_PLAY = 4
+CB_QUIT = -1
+
+class PauseScreen():
+    def __init__(self, screen):
+        self.screen = screen
+
+    def pause_game(self):
+        screen.fill(BLACK)
+        
+        resume_btn = UIElement(
+            center_position=(r.game.SCREEN_WIDTH/2, 450), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
+            font_size=45,
+            bg_rgb=r.colors.BLUE,
+            text_rgb=r.colors.WHITE,
+            text=r.main.r_resume_button_txt,
+            action=CB_PLAY,
+        )
+        quit_btn = UIElement(
+            center_position=(r.game.SCREEN_WIDTH/2, 550),
+            font_size=45,
+            bg_rgb=r.colors.BLUE,
+            text_rgb=r.colors.WHITE,
+            text=r.main.r_quit_button_txt,
+            action=CB_QUIT,
+        )
+
+        buttons = [resume_btn, quit_btn]
+
+        while True:
+            mouse_up = False
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    mouse_up = True
+            self.screen.fill(r.game.BLUE)
+
+            for button in buttons:
+                ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+                if ui_action is not None:
+                    return ui_action
+                button.draw(self.screen)
+
+            pygame.display.flip()
+
