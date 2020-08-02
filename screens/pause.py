f7f1d508b2c79f0e6e8fc1dcfe7f41d66d82df37
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-02 12:37:25 +0530

    Update pause.py

--- screens/pause.py
+++ screens/pause.py
@@ -4,41 +4,58 @@ import r
 
 from sprites.UIElement import *
 
+CB_RETURN = 0
 CB_PLAY = 4
 CB_QUIT = -1
 
 class PauseScreen():
     def __init__(self, screen):
         self.screen = screen
-
-    def pause_game(self):
-        self.screen.fill(r.colors.BLACK)
         
+    def pause_game(self):
+        paused = UIElement(
+            center_position = (r.game.SCREEN_WIDTH/2, 150), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
+            font_size = 90,
+            bg_rgb = r.colors.BLACK,
+            text_rgb = r.colors.WHITE,
+            text = r.main.r_paused_button_txt,
+            action = None,
+        )
         resume_btn = UIElement(
-            center_position=(r.game.SCREEN_WIDTH/2, 450), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
-            font_size=45,
-            bg_rgb=r.colors.BLUE,
-            text_rgb=r.colors.WHITE,
-            text=r.main.r_resume_button_txt,
+            center_position = (r.game.SCREEN_WIDTH/2, 300), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
+            font_size = 45,
+            bg_rgb = r.colors.BLACK,
+            text_rgb = r.colors.WHITE,
+            text = r.main.r_resume_button_txt,
             action=CB_PLAY,
         )
+        return_to_mainmenu_btn = UIElement(
+            center_position = (r.game.SCREEN_WIDTH/2, 400), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
+            font_size = 45,
+            bg_rgb = r.colors.BLACK,
+            text_rgb = r.colors.WHITE,
+            text = r.main.r_return_to_mainmenu_button_txt,
+            action=CB_RETURN,
+        )
         quit_btn = UIElement(
-            center_position=(r.game.SCREEN_WIDTH/2, 550),
-            font_size=45,
-            bg_rgb=r.colors.BLUE,
-            text_rgb=r.colors.WHITE,
-            text=r.main.r_quit_button_txt,
-            action=CB_QUIT,
+            center_position = (r.game.SCREEN_WIDTH/2, 500),
+            font_size = 45,
+            bg_rgb = r.colors.BLACK,
+            text_rgb = r.colors.WHITE,
+            text = r.main.r_quit_button_txt,
+            action = CB_QUIT,
         )
 
-        buttons = [resume_btn, quit_btn]
+        buttons = [paused, resume_btn, quit_btn, return_to_mainmenu_btn]
+
+        paused.setHighlightable(False)
 
         while True:
             mouse_up = False
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                     mouse_up = True
-            self.screen.fill(r.game.BLUE)
+            self.screen.fill(r.game.BLACK)
 
             for button in buttons:
                 ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
