d75e23a415f36a8fde3e4a61cdc238202533b063
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-02 12:36:32 +0530

    Update main.py

--- r/main.py
+++ r/main.py
@@ -8,4 +8,8 @@ r_quit_button_txt = "Quit"
 r_start_button_txt = "Start"
 r_enter_button_txt = "Enter"
 
+r_return_to_mainmenu_button_txt = "Return to Main Menu"
+r_resume_button_txt = "Resume"
+r_paused_button_txt = "PAUSED"
+
 r_playagain_button_txt = "Play Again"
