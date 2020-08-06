261c7077f5ea265255d3ae1a52502b11866d77dc
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-06 13:30:48 +0530

    Update main.py

--- r/main.py
+++ r/main.py
@@ -5,6 +5,13 @@ r_p2_name_label_txt = "Enter the name of Player 2 :"
 r_color_label_txt = "Enter color :"
 r_playernames_label_txt = "Player Names"
 
+r_color_list_label_txt = "LIST OF COLORS :"
+r_color_blue_label_txt = "1. BLUE"
+r_color_green_label_txt = "2. GREEN"
+r_color_yellow_label_txt = "3. YELLOW"
+r_color_orange_label_txt = "4. ORANGE"
+r_color_red_label_txt = "5. RED"
+
 r_quit_button_txt = "Quit"
 r_start_button_txt = "Start"
 r_enter_button_txt = "Enter"
