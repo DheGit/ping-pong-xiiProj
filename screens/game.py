a83b0b8a7cede7cc7ab509a355a5721b6a61bbb5
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-07 12:11:27 +0530

    Update game.py
    Pause bug fixed

--- screens/game.py
+++ screens/game.py
@@ -21,235 +21,231 @@ A class enclosing the game logic. All dimens are tuples (width,height)
 """
 class GameScreen():
 
-	def __init__(self, screen, screen_dimen, score_margin, bg_color, fps):
-		self.screen=screen
-		self.score_margin=score_margin
-		self.screen_dimen=screen_dimen
-		self.bg_color=bg_color
+    def __init__(self, screen, screen_dimen, score_margin, bg_color, fps):
+        self.screen=screen
+        self.score_margin=score_margin
+        self.screen_dimen=screen_dimen
+        self.bg_color=bg_color
 
-		self.reset()
+        self.reset()
 
-		self.fps=fps
+        self.fps=fps
 
-		self.p1Name="Player1"
-		self.p2Name="Player2"
+        self.p1Name="Player1"
+        self.p2Name="Player2"
 
-		self.winnerName="TheHulk" # :P
+        self.winnerName="TheHulk" # :P
 
-		self.game_obj = ""
+        self.game_obj = ""
 
-		self.bounce_acceleration = 1
+        self.bounce_acceleration = 1
 
-	def reset(self):
-		self.score1=0
-		self.score2=0
+    def reset(self):
+        self.score1=0
+        self.score2=0
 
-	def collides(self):
-		if (self.ball.x <= self.paddle1.rect.x + self.paddle_dimen[0] and self.ball.x >= self.paddle1.rect.x + self.paddle_dimen[0] - self.ball.speed*3) and (self.ball.y + self.ball_dimen[1] >= self.paddle1.rect.y and self.ball.y <=self.paddle1.rect.y + self.paddle_dimen[1]):
-			return 1
-		if (self.ball.x >= self.paddle2.rect.x - self.ball.ball_dimen[0] and self.ball.x <= self.paddle2.rect.x - self.ball.ball_dimen[0] + self.ball.speed*3) and (self.ball.y + self.ball_dimen[1] >= self.paddle2.rect.y and self.ball.y <=self.paddle2.rect.y + self.paddle_dimen[1]):
-			return 2
-		return 0
+    def collides(self):
+        if (self.ball.x <= self.paddle1.rect.x + self.paddle_dimen[0] and self.ball.x >= self.paddle1.rect.x + self.paddle_dimen[0] - self.ball.speed*3) and (self.ball.y + self.ball_dimen[1] >= self.paddle1.rect.y and self.ball.y <=self.paddle1.rect.y + self.paddle_dimen[1]):
+            return 1
+        if (self.ball.x >= self.paddle2.rect.x - self.ball.ball_dimen[0] and self.ball.x <= self.paddle2.rect.x - self.ball.ball_dimen[0] + self.ball.speed*3) and (self.ball.y + self.ball_dimen[1] >= self.paddle2.rect.y and self.ball.y <=self.paddle2.rect.y + self.paddle_dimen[1]):
+            return 2
+        return 0
 
-	def play(self):
-		clock = pygame.time.Clock()
+    def play(self):
+        clock = pygame.time.Clock()
 
-		self.ball.setResetMargin(self.ball_reset_margin)
-		self.ball.setBounceBias(self.bounce_bias)
-		self.ball.update()
+        self.ball.setResetMargin(self.ball_reset_margin)
+        self.ball.setBounceBias(self.bounce_bias)
+        self.ball.update()
 
-		movingsprites = pygame.sprite.Group()
-		movingsprites.add(self.paddle1)
-		movingsprites.add(self.paddle2)
-		movingsprites.add(self.ball)
+        movingsprites = pygame.sprite.Group()
+        movingsprites.add(self.paddle1)
+        movingsprites.add(self.paddle2)
+        movingsprites.add(self.ball)
 
-		self.countdown()
-		
-		exit_window = False
-		
-		while not exit_window:
+        self.countdown()
+        
+        exit_window = False
+        
+        while not exit_window:
 
-			self.screen.fill(self.bg_color)
-			
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					exit_window = True
-					return CB_QUIT
+            self.screen.fill(self.bg_color)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit_window = True
+                    return CB_QUIT
 
-			self.ball.update()
-			
-			if self.collides() == 1:
-				diff = (self.paddle1.rect.y + self.paddle_dimen[1]/2) - (self.ball.rect.y+self.ball_dimen[1]/2)
-				self.ball.x = self.paddle_margin+self.paddle_dimen[0] + 2
-				self.ball.bounce(diff)
-				self.ball.speed = self.ball.speed*self.bounce_acceleration
-				
-			if self.collides() == 2:
-				diff = (self.paddle2.rect.y + self.paddle_dimen[1]/2) - (self.ball.rect.y+self.ball_dimen[1]/2) 
-				self.ball.x = self.screen_dimen[0] - (self.paddle_margin+self.ball_dimen[0]+self.paddle_dimen[0]+2)
-				self.ball.bounce(-diff)
-				self.ball.speed = self.ball.speed*self.bounce_acceleration
+            self.ball.update()
+            
+            if self.collides() == 1:
+                diff = (self.paddle1.rect.y + self.paddle_dimen[1]/2) - (self.ball.rect.y+self.ball_dimen[1]/2)
+                self.ball.x = self.paddle_margin+self.paddle_dimen[0] + 2
+                self.ball.bounce(diff)
+                self.ball.speed = self.ball.speed*self.bounce_acceleration
+                
+            if self.collides() == 2:
+                diff = (self.paddle2.rect.y + self.paddle_dimen[1]/2) - (self.ball.rect.y+self.ball_dimen[1]/2) 
+                self.ball.x = self.screen_dimen[0] - (self.paddle_margin+self.ball_dimen[0]+self.paddle_dimen[0]+2)
+                self.ball.bounce(-diff)
+                self.ball.speed = self.ball.speed*self.bounce_acceleration
 
-			if self.ball.crossed(self.paddle_margin):
-				self.score2+=1
-			if self.ball.crossed(self.screen_dimen[0]-self.paddle_margin):
-				self.score1+=1
+            if self.ball.crossed(self.paddle_margin):
+                self.score2+=1
+            if self.ball.crossed(self.screen_dimen[0]-self.paddle_margin):
+                self.score1+=1
 
-			keys = pygame.key.get_pressed()
-			
-			if keys[pygame.K_w]:
-				self.paddle1.moveUp(self.paddle_speed)
-			if keys[pygame.K_s]:
-				self.paddle1.moveDown(self.paddle_speed)
-			if keys[pygame.K_UP]:
-				self.paddle2.moveUp(self.paddle_speed)
-			if keys[pygame.K_DOWN]:
-				self.paddle2.moveDown(self.paddle_speed)
+            keys = pygame.key.get_pressed()
+            
+            if keys[pygame.K_w]:
+                self.paddle1.moveUp(self.paddle_speed)
+            if keys[pygame.K_s]:
+                self.paddle1.moveDown(self.paddle_speed)
+            if keys[pygame.K_UP]:
+                self.paddle2.moveUp(self.paddle_speed)
+            if keys[pygame.K_DOWN]:
+                self.paddle2.moveDown(self.paddle_speed)
 
-			if keys[pygame.K_ESCAPE]:
-				exit_window = True
-				self.reset()
-				return CB_RETURN
-				
-			if keys[pygame.K_F11]:
-				self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+            if keys[pygame.K_ESCAPE]:
+                exit_window = True
+                self.reset()
+                return CB_RETURN
+                
+            if keys[pygame.K_F11]:
+                self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 
-			movingsprites.update()
+            if keys[pygame.K_p]:
+                return CB_PAUSE
 
-			pygame.draw.line(self.screen,colors.WHITE,[self.screen_dimen[0]//2,self.score_margin],[self.screen_dimen[0]//2,self.screen_dimen[1]],5)
+            movingsprites.update()
 
-			pygame.draw.line(self.screen,colors.WHITE,[0,self.score_margin],[self.screen_dimen[0],self.score_margin],5)
+            pygame.draw.line(self.screen,colors.WHITE,[self.screen_dimen[0]//2,self.score_margin],[self.screen_dimen[0]//2,self.screen_dimen[1]],5)
 
-			movingsprites.draw(self.screen)
-			
-			font = pygame.font.Font(None,74)
-			
-			text1 = font.render(str(self.score1),1,colors.WHITE)
-			self.screen.blit(text1,(int(self.screen_dimen[0]/4),10))
-			
-			text2 = font.render(str(self.score2),1,colors.WHITE)
-			self.screen.blit(text2,(3*int(self.screen_dimen[0]/4),10))
+            pygame.draw.line(self.screen,colors.WHITE,[0,self.score_margin],[self.screen_dimen[0],self.score_margin],5)
 
-			if self.score1 == 10 or self.score2 == 10:
-				if self.score1==10:
-					self.winnerName=self.p1Name
-				if self.score2==10:
-					self.winnerName=self.p2Name
-					
-				self.reset()
+            movingsprites.draw(self.screen)
+            
+            font = pygame.font.Font(None,74)
+            
+            text1 = font.render(str(self.score1),1,colors.WHITE)
+            self.screen.blit(text1,(int(self.screen_dimen[0]/4),10))
+            
+            text2 = font.render(str(self.score2),1,colors.WHITE)
+            self.screen.blit(text2,(3*int(self.screen_dimen[0]/4),10))
 
-				self.paddle1.rect.x = self.paddle_margin
-				self.paddle1.rect.y = self.screen_dimen[1]//2 - self.paddle_dimen[1]//2 + self.score_margin//2
+            if self.score1 == 10 or self.score2 == 10:
+                if self.score1==10:
+                    self.winnerName=self.p1Name
+                if self.score2==10:
+                    self.winnerName=self.p2Name
+                    
+                self.reset()
 
-				self.paddle2.rect.x = self.screen_dimen[0] - self.paddle_dimen[0] - self.paddle_margin
-				self.paddle2.rect.y = self.screen_dimen[1]//2 - self.paddle_dimen[1]//2 + self.score_margin//2
-		
-				self.ball.reset()
-				return CB_ENDGAME
+                self.setMovables(BALL_HEIGHT,(PADDLE_WIDTH, PADDLE_HEIGHT), colors.WHITE)
+                                
+                return CB_ENDGAME
 
-			if keys[pygame.K_p]:
-				return CB_PAUSE
+            pygame.display.flip()
 
-			pygame.display.flip()
+            clock.tick(self.fps)
 
-			clock.tick(self.fps)
+    def countdown(self):
+        clock=pygame.time.Clock()
+        three=UIElement(
+            center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2),
+            text="3",
+            font_size=200,
+            bg_rgb=self.bg_color,
+            text_rgb=fg_color_default)
 
-	def countdown(self):
-		clock=pygame.time.Clock()
-		three=UIElement(
-			center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2),
-			text="3",
-			font_size=120,
-			bg_rgb=self.bg_color,
-			text_rgb=fg_color_default)
+        two=UIElement(
+            center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2),
+            text="2",
+            font_size=200,
+            bg_rgb=self.bg_color,
+            text_rgb=fg_color_default)
 
-		two=UIElement(
-			center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2),
-			text="2",
-			font_size=120,
-			bg_rgb=self.bg_color,
-			text_rgb=fg_color_default)
+        one=UIElement(
+            center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2),
+            text="1",
+            font_size=200,
+            bg_rgb=self.bg_color,
+            text_rgb=fg_color_default)
 
-		one=UIElement(
-			center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2),
-			text="1",
-			font_size=120,
-			bg_rgb=self.bg_color,
-			text_rgb=fg_color_default)
+        go=UIElement(
+            center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2),
+            text="GO!",
+            font_size=200,
+            bg_rgb=self.bg_color,
+            text_rgb=fg_color_default)
 
-		go=UIElement(
-			center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2),
-			text="GO!",
-			font_size=120,
-			bg_rgb=self.bg_color,
-			text_rgb=fg_color_default)
+        game_objective=UIElement(
+            center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2+200),
+            text=self.game_obj,
+            font_size=100,
+            bg_rgb=self.bg_color,
+            text_rgb=fg_color_default)
 
-		game_objective=UIElement(
-			center_position=(self.screen_dimen[0]//2,self.screen_dimen[1]//2+100),
-			text=self.game_obj,
-			font_size=70,
-			bg_rgb=self.bg_color,
-			text_rgb=fg_color_default)
+        for i in range(1,self.fps*4+1):
+            self.screen.fill(self.bg_color)
 
-		for i in range(1,self.fps*4+1):
-			self.screen.fill(self.bg_color)
+            game_objective.draw(self.screen)
+            num=4-i//self.fps
+            if num==1:
+                go.draw(self.screen)
+            elif num==2:
+                one.draw(self.screen)
+            elif num==3:
+                two.draw(self.screen)
+            elif num==4:
+                three.draw(self.screen)
 
-			game_objective.draw(self.screen)
-			num=4-i//self.fps
-			if num==1:
-				go.draw(self.screen)
-			elif num==2:
-				one.draw(self.screen)
-			elif num==3:
-				two.draw(self.screen)
-			elif num==4:
-				three.draw(self.screen)
+            pygame.display.flip()
+            
+            clock.tick(self.fps)
 
-			pygame.display.flip()
-			
-			clock.tick(self.fps)
+    def setGameObjective(self, game_obj):
+        self.game_obj=game_obj
 
-	def setGameObjective(self, game_obj):
-		self.game_obj=game_obj
+    def setMovables(self, ball_radius, paddle_dimen, color):
+        self.paddle_dimen=paddle_dimen
+        self.ball_dimen=(ball_radius,ball_radius)
 
-	def setMovables(self, ball_radius, paddle_dimen, color):
-		self.paddle_dimen=paddle_dimen
-		self.ball_dimen=(ball_radius,ball_radius)
+        self.ball=Ball(self.ball_dimen, self.screen_dimen, self.paddle_dimen, self.score_margin)
+        self.ball_group=pygame.sprite.Group()
+        self.ball_group.add(self.ball)
 
-		self.ball=Ball(self.ball_dimen, self.screen_dimen, self.paddle_dimen, self.score_margin)
-		self.ball_group=pygame.sprite.Group()
-		self.ball_group.add(self.ball)
+        self.paddle1=Paddle(self.screen_dimen, self.paddle_dimen, self.score_margin)
+        self.paddle2=Paddle(self.screen_dimen, self.paddle_dimen, self.score_margin)
 
-		self.paddle1=Paddle(self.screen_dimen, self.paddle_dimen, self.score_margin)
-		self.paddle2=Paddle(self.screen_dimen, self.paddle_dimen, self.score_margin)
+        self.paddle1.rect.x = PADDLE_MARGIN
+        self.paddle1.rect.y = SCREEN_HEIGHT//2 - PADDLE_HEIGHT//2 + SCORE_MARGIN//2
 
-		self.paddle1.rect.x = PADDLE_MARGIN
-		self.paddle1.rect.y = SCREEN_HEIGHT//2 - PADDLE_HEIGHT//2 + SCORE_MARGIN//2
+        self.paddle2.rect.x = SCREEN_WIDTH - PADDLE_WIDTH - PADDLE_MARGIN
+        self.paddle2.rect.y = SCREEN_HEIGHT//2 - PADDLE_HEIGHT//2 + SCORE_MARGIN//2
 
-		self.paddle2.rect.x = SCREEN_WIDTH - PADDLE_WIDTH - PADDLE_MARGIN
-		self.paddle2.rect.y = SCREEN_HEIGHT//2 - PADDLE_HEIGHT//2 + SCORE_MARGIN//2
+        self.ball.reset()
 
+    def setPaddleMargin(self, paddle_margin):
+        self.paddle_margin=paddle_margin
 
-	def setPaddleMargin(self, paddle_margin):
-		self.paddle_margin=paddle_margin
+    def setPaddleSpeed(self, paddle_speed):
+        self.paddle_speed=paddle_speed
 
-	def setPaddleSpeed(self, paddle_speed):
-		self.paddle_speed=paddle_speed
+    def setBallResetMargin(self, ball_reset_margin):
+        self.ball_reset_margin=ball_reset_margin
 
-	def setBallResetMargin(self, ball_reset_margin):
-		self.ball_reset_margin=ball_reset_margin
+    def setBounceBias(self, bounce_bias):
+        self.bounce_bias=bounce_bias
 
-	def setBounceBias(self, bounce_bias):
-		self.bounce_bias=bounce_bias
+    def setPlayer1Name(self, p1Name):
+        self.p1Name=p1Name
 
-	def setPlayer1Name(self, p1Name):
-		self.p1Name=p1Name
+    def setPlayer1Name(self, p1Name):
+        self.p2Name=p2Name
 
-	def setPlayer1Name(self, p1Name):
-		self.p2Name=p2Name
+    def setBounceAcceleration(self, bounce_acceleration):
+        self.bounce_acceleration = bounce_acceleration
 
-	def setBounceAcceleration(self, bounce_acceleration):
-		self.bounce_acceleration = bounce_acceleration
-
-	def getWinnerName(self):
-		return self.winnerName
+    def getWinnerName(self):
+        return self.winnerName
