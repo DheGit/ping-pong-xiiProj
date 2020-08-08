ecdadad524b3d74535c3cbb1ec3b6cffc7c6ef8b
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-08 18:25:40 +0530

    Update __init__.py

--- sprites/__init__.py
+++ sprites/__init__.py
@@ -1,5 +1,6 @@
 from . import Ball
 from . import Paddle
 from . import UIElement
+from . import Textbox
 
-__all__ = ['Ball', 'Paddle', 'UIElement']
+__all__ = ['Ball', 'Paddle', 'UIElement', 'Textbox']
