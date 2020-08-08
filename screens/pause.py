75d12d9c0abd947bf010798f335220d50e16e3ba
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-08 20:31:44 +0530

    Update pause.py

--- screens/pause.py
+++ screens/pause.py
@@ -15,31 +15,31 @@ class PauseScreen():
     def pause_game(self):
         paused = UIElement(
             center_position = (r.game.SCREEN_WIDTH/2, 150), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
-            font_size = 90,
+            font_size = 125,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = r.main.r_paused_button_txt,
             action = None,
         )
         resume_btn = UIElement(
-            center_position = (r.game.SCREEN_WIDTH/2, 300), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
-            font_size = 45,
+            center_position = (r.game.SCREEN_WIDTH/2, 315), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
+            font_size = 60,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = r.main.r_resume_button_txt,
             action=CB_PLAY,
         )
         return_to_mainmenu_btn = UIElement(
-            center_position = (r.game.SCREEN_WIDTH/2, 400), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
-            font_size = 45,
+            center_position = (r.game.SCREEN_WIDTH/2, 415), #TODO: Change these hardcoded values into variables, including margin, etc, to make the positioning more comfortable and dynamic 
+            font_size = 60,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = r.main.r_return_to_mainmenu_button_txt,
             action=CB_RETURN,
         )
         quit_btn = UIElement(
-            center_position = (r.game.SCREEN_WIDTH/2, 500),
-            font_size = 45,
+            center_position = (r.game.SCREEN_WIDTH/2, 515),
+            font_size = 60,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = r.main.r_quit_button_txt,
