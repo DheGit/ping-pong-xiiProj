54b0e1a6ecf74862e4c3bf480ed92efa94eb2cd7
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-08 20:31:17 +0530

    Update main.py

--- r/main.py
+++ r/main.py
@@ -1,23 +1,24 @@
 r_title_label_txt = "Ping-Pong"
 
-r_p1_name_label_txt = "Enter the name of Player 1 :"
-r_p2_name_label_txt = "Enter the name of Player 2 :"
-r_color_label_txt = "Enter color :"
-r_playernames_label_txt = "Player Names"
-
-r_color_list_label_txt = "LIST OF COLORS :"
-r_color_blue_label_txt = "1. BLUE"
-r_color_green_label_txt = "2. GREEN"
-r_color_yellow_label_txt = "3. YELLOW"
-r_color_orange_label_txt = "4. ORANGE"
-r_color_red_label_txt = "5. RED"
-
-r_quit_button_txt = "Quit"
 r_start_button_txt = "Start"
+r_quit_button_txt = "Quit"
+
+r_playernames_label_txt = "Player Names"
+r_p1_label_txt = "Player 1"
+r_p2_label_txt = "Player 2"
+r_name_label_txt = "Name :"
+r_color_blue_label_txt = "Blue"
+r_color_green_label_txt = "Green"
+r_color_yellow_label_txt = "Yellow"
+r_color_orange_label_txt = "Orange"
+r_color_red_label_txt = "Red"
+
 r_enter_button_txt = "Enter"
 
-r_return_to_mainmenu_button_txt = "Return to Main Menu"
-r_resume_button_txt = "Resume"
+r_pause_button = "⏸️"
+
 r_paused_button_txt = "PAUSED"
+r_resume_button_txt = "Resume"
+r_return_to_mainmenu_button_txt = "Return to Main Menu"
 
 r_playagain_button_txt = "Play Again"
