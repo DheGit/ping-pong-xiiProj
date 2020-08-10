309d0b1410cb05928f830c234f42a8f59b41c6e2
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-10 13:48:33 +0530

    Update main.py

--- r/main.py
+++ r/main.py
@@ -2,23 +2,3 @@ r_title_label_txt = "Ping-Pong"
 
 r_start_button_txt = "Start"
 r_quit_button_txt = "Quit"
-
-r_playernames_label_txt = "Player Names"
-r_p1_label_txt = "Player 1"
-r_p2_label_txt = "Player 2"
-r_name_label_txt = "Name :"
-r_color_blue_label_txt = "Blue"
-r_color_green_label_txt = "Green"
-r_color_yellow_label_txt = "Yellow"
-r_color_pink_label_txt = "Pink"
-r_color_red_label_txt = "Red"
-
-r_enter_button_txt = "Enter"
-
-r_pause_button = "⏸️"
-
-r_paused_button_txt = "PAUSED"
-r_resume_button_txt = "Resume"
-r_return_to_mainmenu_button_txt = "Return to Main Menu"
-
-r_playagain_button_txt = "Play Again"
