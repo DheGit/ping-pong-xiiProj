f2484eea93ff945e792cfd472ed8dab3db959a19
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-15 13:48:25 +0530

    Update pause.py

--- screens/pause.py
+++ screens/pause.py
@@ -2,7 +2,7 @@ import pygame
 
 import r
 
-from sprites.UIElement import *
+from sprites.Button import *
 
 CB_RETURN = 0
 CB_PLAY = 4
@@ -12,50 +12,9 @@ class PauseScreen():
     def __init__(self, screen):
         self.screen = screen
         self.scores=(0,0)
-        self.setUI()
+        self.setButtons()
         
     def pause_game(self):
-        # paused = UIElement(
-        #     center_position = (r.game.SCREEN_WIDTH/2, 125),
-        #     font_size = 125,
-        #     bg_rgb = r.colors.BLACK,
-        #     text_rgb = r.colors.WHITE,
-        #     text = r.pause.paused_label_txt,
-        #     action = None,
-        # )
-        # score_label=UIElement(
-        #     center_position = (r.game.SCREEN_WIDTH/2, 285),
-        #     font_size = 60,
-        #     bg_rgb = r.colors.BLACK,
-        #     text_rgb = r.colors.WHITE,
-        #     text = str(self.scores[0])+" : "+str(self.scores[1]),
-        #     action = None,
-        # )
-        # resume_btn = UIElement(
-        #     center_position = (r.game.SCREEN_WIDTH/2, 385),
-        #     font_size = 60,
-        #     bg_rgb = r.colors.BLACK,
-        #     text_rgb = r.colors.WHITE,
-        #     text = r.pause.resume_button_txt,
-        #     action=CB_PLAY,
-        # )
-        # return_to_mainmenu_btn = UIElement(
-        #     center_position = (r.game.SCREEN_WIDTH/2, 485),
-        #     font_size = 60,
-        #     bg_rgb = r.colors.BLACK,
-        #     text_rgb = r.colors.WHITE,
-        #     text = r.pause.return_to_mainmenu_button_txt,
-        #     action=CB_RETURN,
-        # )
-        # quit_btn = UIElement(
-        #     center_position = (r.game.SCREEN_WIDTH/2, 585),
-        #     font_size = 60,
-        #     bg_rgb = r.colors.BLACK,
-        #     text_rgb = r.colors.WHITE,
-        #     text = r.pause.quit_button_txt,
-        #     action = CB_QUIT,
-        # )
-
         buttons = [self.score_label, self.paused, self.resume_btn, self.quit_btn, self.return_to_mainmenu_btn]
 
         self.paused.setHighlightable(False)
@@ -69,9 +28,9 @@ class PauseScreen():
             self.screen.fill(r.game.BLACK)
 
             for button in buttons:
-                ui_action = button.update(pygame.mouse.get_pos(), mouse_up)                    
-                if ui_action is not None:
-                    return ui_action
+                button_action = button.update(pygame.mouse.get_pos(), mouse_up)                    
+                if button_action is not None:
+                    return button_action
                 button.draw(self.screen)
 
                 keys = pygame.key.get_pressed()
@@ -80,42 +39,42 @@ class PauseScreen():
 
             pygame.display.flip()
 
-    def setUI(self):
-        self.paused = UIElement(
+    def setButtons(self):
+        self.paused = Button(
             center_position = (r.game.SCREEN_WIDTH/2, 125),
-            font_size = 125,
+            font_size = r.font_size.xxl,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = r.pause.paused_label_txt,
             action = None,
         )
-        self.score_label=UIElement(
+        self.score_label=Button(
             center_position = (r.game.SCREEN_WIDTH/2, 285),
-            font_size = 60,
+            font_size = r.font_size.m,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = str(self.scores[0])+" : "+str(self.scores[1]),
             action = None,
         )
-        self.resume_btn = UIElement(
+        self.resume_btn = Button(
             center_position = (r.game.SCREEN_WIDTH/2, 385),
-            font_size = 60,
+            font_size = r.font_size.m,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = r.pause.resume_button_txt,
             action=CB_PLAY,
         )
-        self.return_to_mainmenu_btn = UIElement(
+        self.return_to_mainmenu_btn = Button(
             center_position = (r.game.SCREEN_WIDTH/2, 485),
-            font_size = 60,
+            font_size = r.font_size.m,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = r.pause.return_to_mainmenu_button_txt,
             action=CB_RETURN,
         )
-        self.quit_btn = UIElement(
+        self.quit_btn = Button(
             center_position = (r.game.SCREEN_WIDTH/2, 585),
-            font_size = 60,
+            font_size = r.font_size.m,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = r.pause.quit_button_txt,
@@ -124,9 +83,9 @@ class PauseScreen():
 
     def setScores(self,a):
         self.scores=a
-        self.score_label=UIElement(
+        self.score_label=Button(
             center_position = (r.game.SCREEN_WIDTH/2, 285),
-            font_size = 60,
+            font_size = r.font_size.m,
             bg_rgb = r.colors.BLACK,
             text_rgb = r.colors.WHITE,
             text = str(self.scores[0])+" : "+str(self.scores[1]),
