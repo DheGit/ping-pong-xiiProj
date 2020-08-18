ec11843cfd510c407d44877b6e17cb8bf8f94a1c
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-19 00:01:24 +0530

    Update The Game.py

--- The Game.py
+++ The Game.py
@@ -12,7 +12,7 @@ def main():
     pygame.display.set_caption(r.main.r_title_label_txt)
 
     screen = pygame.display.set_mode((r.game.SCREEN_WIDTH, r.game.SCREEN_HEIGHT))
-    game_state = GameState.MENU
+    game_state = GameScreen.MENU
 
     game=screens.game.GameScreen(screen, (r.game.SCREEN_WIDTH, r.game.SCREEN_HEIGHT), r.game.SCORE_MARGIN, r.colors.BLACK, r.game.FPS)
     game.setPaddleMargin(r.game.PADDLE_MARGIN)
@@ -34,84 +34,84 @@ def main():
     about_screen=screens.about.AboutScreen(screen,r.about.text_about,(r.game.SCREEN_WIDTH, r.game.SCREEN_HEIGHT),r.colors.BLACK,r.colors.WHITE,r.game.FPS,fontsize=r.font_size.s)
 
     while True:
-        if game_state == GameState.MENU:
+        if game_state == GameScreen.MENU:
             game_state = start_menu(screen)
 
-        if game_state == GameState.PLAYERNAMES:
+        if game_state == GameScreen.PLAYERNAMES:
             game_state = names(screen)
 
-        if game_state == GameState.PLAYGAME:
+        if game_state == GameScreen.PLAYGAME:
             game_state = start_game(screen, game)
 
-        if game_state == GameState.PAUSE:
+        if game_state == GameScreen.PAUSE:
             game_state = pause_game(screen)
             
-        if game_state == GameState.ENDGAME:
+        if game_state == GameScreen.ENDGAME:
             game_state = launch_endgame(screen)
 
-        if game_state == GameState.ABOUT:
+        if game_state == GameScreen.ABOUT:
             game_state = launchAbout(screen)
 
-        if game_state == GameState.QUIT:
+        if game_state == GameScreen.QUIT:
             pygame.quit()
             return
 
 def start_menu(screen):
-    new_state=main_menu.show_menu()
+    new_screen=main_menu.show_menu()
 
     game.reset()
 
-    if new_state == screens.main_menu.CB_QUIT:
-        return GameState.QUIT
-    if new_state == screens.main_menu.CB_NAMES:
-        return GameState.PLAYERNAMES
-    if new_state == screens.main_menu.CB_ABOUT:
-        return GameState.ABOUT
+    if new_screen == screens.main_menu.CB_QUIT:
+        return GameScreen.QUIT
+    if new_screen == screens.main_menu.CB_NAMES:
+        return GameScreen.PLAYERNAMES
+    if new_screen == screens.main_menu.CB_ABOUT:
+        return GameScreen.ABOUT
 
-    return GameState.QUIT
+    return GameScreen.QUIT
 
 def names(screen):
-    new_state=player_names.names()
+    new_screen=player_names.names()
 
-    if new_state == screens.playernames.CB_PLAY:
+    if new_screen == screens.playernames.CB_PLAY:
         game.setPlayer1Name(player_names.getPlayer1Name())
         game.setPlayer2Name(player_names.getPlayer2Name())
         game.setMovables(r.game.BALL_HEIGHT, (r.game.PADDLE_WIDTH, r.game.PADDLE_HEIGHT), player_names.getColor1(), player_names.getColor2())
-        return GameState.PLAYGAME
-    if new_state == screens.playernames.CB_RETURN:
-        return GameState.MENU
+        return GameScreen.PLAYGAME
+    if new_screen == screens.playernames.CB_RETURN:
+        return GameScreen.MENU
 
-    return GameState.QUIT
+    return GameScreen.QUIT
 
 def start_game(screen,game):
-    new_state = game.play()
+    new_screen = game.play()
 
-    if new_state == screens.game.CB_PAUSE:
-        return GameState.PAUSE
-    elif new_state == screens.game.CB_ENDGAME:
-        return GameState.ENDGAME
-    elif new_state == screens.game.CB_RETURN:
-        return GameState.MENU
-    elif new_state == screens.game.CB_QUIT:
-        return GameState.QUIT
+    if new_screen == screens.game.CB_PAUSE:
+        return GameScreen.PAUSE
+    elif new_screen == screens.game.CB_ENDGAME:
+        return GameScreen.ENDGAME
+    elif new_screen == screens.game.CB_RETURN:
+        return GameScreen.MENU
+    elif new_screen == screens.game.CB_QUIT:
+        return GameScreen.QUIT
     
-    return GameState.MENU
+    return GameScreen.MENU
 
 def pause_game(screen):
     global game,pause_screen
 
     pause_screen.setScores(game.getScores())
-    new_state = pause_screen.pause_game()
+    new_screen = pause_screen.pause_game()
 
 
-    if new_state == screens.pause.CB_QUIT:
-        return GameState.QUIT
-    if new_state == screens.pause.CB_PLAY:
-        return GameState.PLAYGAME
-    if new_state == screens.game.CB_RETURN:
-        return GameState.MENU
+    if new_screen == screens.pause.CB_QUIT:
+        return GameScreen.QUIT
+    if new_screen == screens.pause.CB_PLAY:
+        return GameScreen.PLAYGAME
+    if new_screen == screens.game.CB_RETURN:
+        return GameScreen.MENU
 
-    return GameState.MENU
+    return GameScreen.MENU
 
 def launch_endgame(screen):
     global endgame_screen,game
@@ -119,31 +119,31 @@ def launch_endgame(screen):
     endgame_screen.setWinnerName(game.getWinnerName())
     endgame_screen.setWinnerColor(game.getWinnerColor())
 
-    new_state=endgame_screen.showEndScreen()
+    new_screen=endgame_screen.showEndScreen()
 
-    if new_state==screens.endgame.CB_PLAY:
-        return GameState.PLAYGAME
-    if new_state==screens.endgame.CB_RETURN:
-        return GameState.MENU
-    if new_state == screens.pause.CB_QUIT:
-        return GameState.QUIT
+    if new_screen==screens.endgame.CB_PLAY:
+        return GameScreen.PLAYGAME
+    if new_screen==screens.endgame.CB_RETURN:
+        return GameScreen.MENU
+    if new_screen == screens.pause.CB_QUIT:
+        return GameScreen.QUIT
 
-    return GameState.MENU
+    return GameScreen.MENU
 
 def launchAbout(screen):
     global about_screen
 
-    new_state=about_screen.showAbout()
+    new_screen=about_screen.showAbout()
 
-    if new_state==screens.about.CB_RETURN:
-        return GameState.MENU
-    if new_state==screens.about.CB_QUIT:
-        return GameState.QUIT
+    if new_screen==screens.about.CB_RETURN:
+        return GameScreen.MENU
+    if new_screen==screens.about.CB_QUIT:
+        return GameScreen.QUIT
 
-    return GameState.MENU
+    return GameScreen.MENU
 
 
-class GameState(Enum):
+class GameScreen(Enum):
     QUIT=-1
     MENU=0
     PLAYGAME=1
