a036bab66ba14815cad286ea77ec474628a83717
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-19 00:00:46 +0530

    Update about.py

--- r/about.py
+++ r/about.py
@@ -1,3 +1,3 @@
-text_about = "This is a pygame-based class 12 project made by Dev Radadia and Dheer Banker.\n\nUse W,S and Up,Down(Arrow) to move, P to pause, Esc to Quit. Enjoy!"
+text_about = "This is a Pygame-Based Class-12 Project made by Dev Radadia and Dheer Banker.\n\nUse W,S and Up,Down(Arrow) to Move, P to Pause, Esc to Quit. Enjoy!"
 
 txt_return_btn="Return to Main Menu"
