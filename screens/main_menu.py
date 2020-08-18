2cf8c26e243ee3c903a3b2534a49da0315bd034e
DheGit <kevindheer@gmail.com>
2020-08-18 20:58:26 +0530

    added an about button to the main menu

--- screens/main_menu.py
+++ screens/main_menu.py
@@ -21,8 +21,16 @@ class MainMenuScreen():
 	        text=r.main.r_start_button_txt,
 	        action=CB_NAMES,
 	    )
+	    about_btn=Button( 
+	        center_position=(r.game.SCREEN_WIDTH/2, 515),
+	        font_size=r.font_size.m,
+	        bg_rgb=r.colors.BLACK,
+	        text_rgb=r.colors.WHITE,
+	        text=r.main.r_about_button_txt,
+	        action=CB_ABOUT,
+		)
 	    quit_btn = Button(
-	        center_position=(r.game.SCREEN_WIDTH/2, 550),
+	        center_position=(r.game.SCREEN_WIDTH/2, 600),
 	        font_size=r.font_size.m,
 	        bg_rgb=r.colors.BLACK,
 	        text_rgb=r.colors.WHITE,
@@ -40,7 +48,7 @@ class MainMenuScreen():
 
 	    game_name.setHighlightable(False)
 
-	    buttons = [start_btn, quit_btn, game_name]
+	    buttons = [start_btn, about_btn, quit_btn, game_name]
 
 
 	    while True:
