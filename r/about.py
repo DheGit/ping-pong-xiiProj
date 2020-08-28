f33e4d50eb31cab41f9eca86e932930e299e465d
DheGit <kevindheer@gmail.com>
2020-08-28 19:20:19 +0530

    changed about text a bit

--- r/about.py
+++ r/about.py
@@ -1,3 +1,3 @@
-text_about = "This is a Pygame-Based Class-12 Project made by Dev Radadia and Dheer Banker.\n\nChoose your paddle color, use [W,S] and [Up,Down](Arrow) to move, P to Pause, Esc to Quit and Play On!"
+text_about="This is a Pygame-Based Class-12 Project made by Dev Radadia and Dheer Banker.\n\nControls: \nW and S: To move left paddle Up and Down\nUp and Down(Arrows): To move right paddle Up and Down\n\nChoose your paddle colour, and play on! "
 
 txt_return_btn="Return to Main Menu"
