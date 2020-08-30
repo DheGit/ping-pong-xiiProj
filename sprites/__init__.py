7a14a10d708901d6bcc87d2882856dabaef135ce
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-30 19:51:22 +0530

    Update __init__.py

--- sprites/__init__.py
+++ sprites/__init__.py
@@ -3,5 +3,6 @@ from . import Paddle
 from . import Button
 from . import Textbox
 from . import Label
+from . import PauseButton
 
-__all__ = ['Ball', 'Paddle', 'Button', 'Textbox','Label']
+__all__ = ['Ball', 'Paddle', 'Button', 'Textbox', 'Label', 'PauseButton']
