7d5cce055b54dbc7f5648eb240e9b93febcd4d1e
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-08-31 19:31:14 +0530

    Update game.py

--- screens/game.py
+++ screens/game.py
@@ -88,12 +88,12 @@ class GameScreen():
 
         # pausebutton = PauseButton(action = CB_PAUSE)
         pausebutton=Button( 
-            center_position=(r.game.SCREEN_WIDTH/2, 30),
-            font_size=r.font_size.xs,
-            bg_rgb=r.colors.BLACK,
-            text_rgb=r.colors.WHITE,
-            text=r.game.pause_button_txt,
-            action=CB_PAUSE,
+            center_position = (self.screen_dimen[0]/2, self.score_margin/2),
+            font_size = r.font_size.xs,
+            bg_rgb = r.colors.BLACK,
+            text_rgb = r.colors.WHITE,
+            text = r.game.pause_button_txt,
+            action = CB_PAUSE,
         )
         buttons = [pausebutton]
 
@@ -117,6 +117,10 @@ class GameScreen():
                 if button_action is not None:
                     print("Drawing Button")
                     return button_action
+
+                pygame.draw.circle(self.screen,r.colors.WHITE,[self.screen_dimen[0]//2,self.score_margin//2],30)
+                pygame.draw.circle(self.screen,r.colors.BLACK,[self.screen_dimen[0]//2,self.score_margin//2],28)
+                
                 button.draw(self.screen)
 
             self.border.rectangle(self.screen)
