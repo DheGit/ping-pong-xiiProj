8b03436de596032e0935f8559ccaa96d5f37c1b8
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-10-06 10:51:52 +0530

    Update Border.py

--- sprites/Border.py
+++ sprites/Border.py
@@ -4,7 +4,7 @@ import r
 class Border(pygame.sprite.Sprite):
 
     def rectangle(self, screen):
-        pygame.draw.line(screen, r.colors.WHITE, [0,0],[r.game.SCREEN_WIDTH,0], 5)
-        pygame.draw.line(screen, r.colors.WHITE, [r.game.SCREEN_WIDTH,0],[r.game.SCREEN_WIDTH,r.game.SCREEN_HEIGHT], 5)
-        pygame.draw.line(screen, r.colors.WHITE, [r.game.SCREEN_WIDTH,r.game.SCREEN_HEIGHT],[0,r.game.SCREEN_HEIGHT], 5)
-        pygame.draw.line(screen, r.colors.WHITE, [0,r.game.SCREEN_HEIGHT],[0,0], 5)
+        pygame.draw.line(screen, r.colors.WHITE, [0,0],[r.game.SCREEN_WIDTH,0], 9)
+        pygame.draw.line(screen, r.colors.WHITE, [r.game.SCREEN_WIDTH,0],[r.game.SCREEN_WIDTH,r.game.SCREEN_HEIGHT], 9)
+        pygame.draw.line(screen, r.colors.WHITE, [r.game.SCREEN_WIDTH,r.game.SCREEN_HEIGHT],[0,r.game.SCREEN_HEIGHT], 9)
+        pygame.draw.line(screen, r.colors.WHITE, [0,r.game.SCREEN_HEIGHT],[0,0], 9)
