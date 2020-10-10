678dbb210b141e5aae7954cb4eab7f761f26996f
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-10-10 08:42:16 +0530

    Update about.py

--- r/about.py
+++ r/about.py
@@ -1,3 +1,3 @@
-text_about="This is a Pygame-Based Class-12 Project made by Dev Radadia and Dheer Banker.\n\n\nControls :-\nW and S : To move Left paddle Up and Down\nUp and Down(Arrows) : To move Right paddle Up and Down\n\n\nChoose your paddle colour, and PLAY ON !!! "
+text_about="This is a Pygame-Based Class-12 Project made by Dev Radadia and Dheer Banker.\n\n\nControls :-\nW and S : To move Left paddle Up and Down\nUp and Down (Arrows) : To move Right paddle Up and Down\n\n\nSet your name, choose your paddle colour, and PLAY ON !!! "
 
 txt_return_btn="Return to Main Menu"
