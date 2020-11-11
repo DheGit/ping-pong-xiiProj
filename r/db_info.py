1e22bd8dcaef559f5274e21a6f29dee43e5ea64e
DheGit <kevindheer@gmail.com>
2020-11-11 11:17:49 +0530

    changed the username and password to something generic

--- r/db_info.py
+++ r/db_info.py
@@ -1,8 +1,8 @@
 #Stores the information of the database
 
 HOST="localhost"
-USER="DrDb"
-PASS="DrDb_root"
+USER="root"
+PASS="pswd_root"
 DBNAME="pongdata"
 
 TB_GAMESTAT = "gamestat" #Name of the table storing game statuses 
