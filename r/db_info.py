fb760d0bd2fab2e5c4c26412f1f6f61ad106c4f1
Dra123cula <67581983+Dra123cula@users.noreply.github.com>
2020-11-12 10:57:50 +0530

    Update db_info.py

--- r/db_info.py
+++ r/db_info.py
@@ -1,15 +1,14 @@
-#Stores the information of the database
+HostName = "localhost"
+UserName = "root"
+Password = "knowledge"	# Type your MySQL Password here
+DatabaseName = "PongData"
 
-HOST="localhost"
-USER="root" # Place the respective
-PASS="pass"	# credentials here.
-DBNAME="pongdata"
+TableName = "GameStats"
 
-TB_GAMESTAT = "gamestat" #Name of the table storing game statuses 
-C_ID="GameId"
-C_WNAME="Winner"
-C_LNAME="Loser"
+C_ID = "GameId"
+C_WINNER = "Winner"
+C_LOSER = "Loser"
 
-Q_CREATE_PONGDATA="CREATE DATABASE "+DBNAME
-Q_CREATE_GSTAT="CREATE TABLE "+TB_GAMESTAT+"("+C_ID+" INT AUTO_INCREMENT PRIMARY KEY, "+C_WNAME+" VARCHAR(32), "+C_LNAME+" VARCHAR(32))"
-Q_ADD_GAME_DATA="INSERT INTO "+TB_GAMESTAT+" ("+C_WNAME+", "+C_LNAME+") VALUES ('{}', '{}')"
+Q_CREATE_PONGDATA = "CREATE DATABASE "+DatabaseName
+Q_CREATE_GAMESTATS = "CREATE TABLE "+TableName+"("+C_ID+" INT AUTO_INCREMENT PRIMARY KEY, "+C_WINNER+" VARCHAR(32), "+C_LOSER+" VARCHAR(32))"
+Q_ADD_GAME_DATA = "INSERT INTO "+TableName+" ("+C_WINNER+", "+C_LOSER+") VALUES ('{}', '{}')"
